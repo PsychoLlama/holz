77a35492a4d71020ccada55f8f82f8906e72ea7d
Jesse Gibson <JesseTheGibson@gmail.com>
2023-02-21 03:03:50 -0500

    Use environment filter in default bundle
    One step closer to creating a modern clone of `debug`.

--- packages/holz-logger/src/index.browser.ts
+++ packages/holz-logger/src/index.browser.ts
@@ -1,6 +1,9 @@
 import { ConsoleBackend } from '@holz/console-backend';
 import { createLogger } from '@holz/core';
+import { EnvironmentFilter } from '@holz/env-filter';
 
-const logger = createLogger(new ConsoleBackend());
+const logger = createLogger(
+  new EnvironmentFilter({ processor: new ConsoleBackend() })
+);
 
 export default logger;
--- packages/holz-logger/src/index.node.ts
+++ packages/holz-logger/src/index.node.ts
@@ -1,6 +1,11 @@
 import { AnsiTerminalBackend } from '@holz/ansi-terminal-backend';
 import { createLogger } from '@holz/core';
+import { EnvironmentFilter } from '@holz/env-filter';
 
-const logger = createLogger(new AnsiTerminalBackend());
+const logger = createLogger(
+  new EnvironmentFilter({
+    processor: new AnsiTerminalBackend(),
+  })
+);
 
 export default logger;
