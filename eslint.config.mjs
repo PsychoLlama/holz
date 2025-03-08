aec97d3efb537ea51adde67be18299d35cb0023d
Jesse Gibson <JesseTheGibson@gmail.com>
2025-03-08 11:36:59 -0500

    Upgrade to ESLint v9
    This migrates to the v9 flat config format. It's painful every time.
    
    I also enabled typed lint rules for ts-eslint which revealed some new
    and valid warnings.

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+// This package doesn't have type definitions.
+// @ts-expect-error
+import importPlugin from 'eslint-plugin-import';
+
+export default tseslint.config(
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'import/order': ['error'],
+    },
+  },
+  {
+    files: ['./**/__tests__/*.ts{x,}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+);
